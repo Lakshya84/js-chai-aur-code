8907dfeb0c354c57a367b95ed487f88e557e1414
Lakshya Pandey <104685907+Lakshya84@users.noreply.github.com>
2024-05-07 18:32:59 +0000

    container config done

---
+++ basics_01/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "lakshya78@gmail.com"
+var accountPassword ="256215"
+accountCity = "Ksnpur"
+let accountNumber
+
+
+console.log(accountEmail);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountNumber])
+
+// accountId="589631423"
+
+/* Note: let use karna hai var use nhi krna hai kyuki usme "block scope" ka issue hota hai*/
